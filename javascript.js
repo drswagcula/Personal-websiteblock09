80ff392ec77b17e441d70f1af2878b09ebd62ed1
Drswagcula <Cajukiku@gmail.com>
2025-01-16 23:05:39 -0600

    lee more

--- javascript.js
+++ javascript.js
@@ -8,3 +8,7 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   window.location.href = window.location.origin + '/success.html'
 })
+
+setTimeout(() => {
+    window.location.href = window.location.origin
+  }, 5000)
