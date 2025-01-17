7722db257d8ce983b1444e6d464526ab8f2ea05a
Drswagcula <Cajukiku@gmail.com>
2025-01-16 21:35:33 -0600

    Website wow

--- javascript.js
+++ javascript.js
@@ -1,2 +1,10 @@
 // javascript.js
 console.log("Hello World");
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
