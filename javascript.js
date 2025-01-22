06284611dd68b4ce5ad2cea9bda7ccc8c91e6a0a
Drswagcula <Cajukiku@gmail.com>
2025-01-21 20:43:08 -0600

    final touches

--- javascript.js
+++ javascript.js
@@ -6,9 +6,5 @@ const submitButton = document.querySelector('#submit')
 form.addEventListener('submit', (e) => {
   submitButton.disabled = true
   e.preventDefault()
-  window.location.href = window.location.origin + '/success.html'
+  window.location.href = window.location.origin + '/formsubmitted.html'
 })
-
-setTimeout(() => {
-    window.location.href = window.location.origin
-  }, 5000)
